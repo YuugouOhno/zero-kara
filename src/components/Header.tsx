02f164d51083aba6cb2fa6cec96a0d1130aa21db
YuugouOhno <yuugou.ohno@gmail.com>
2025-02-15 23:59:18 +0900

    ファビコン、ヘッダーアイコンを設定

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,18 @@
 import { ThemeSwitch } from "@/feature/theme/theme-switch";
 import Link from "next/link";
+import Image from "next/image";
 
 const Header = () => {
 
     return (
         <header className="h-12 w-full z-50 bg-original-purple text-original-white font-bold fixed flex justify-between items-center px-4">
-            <Link href="/">
-                <div>©0→ ゼロカラ</div>
+            <Link href="/" className="relative h-10 w-28">
+                <Image
+                    src="/header_icon.svg"
+                    fill
+                    alt="header_icon"
+                    className="object-cover rounded-lg"
+                />
             </Link>
             <ThemeSwitch />
         </header>
