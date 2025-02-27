b5127accc23bdd6bb2fc53d95ad0615677689b56
YuugouOhno <yuugou.ohno@gmail.com>
2025-02-27 11:37:27 -0500

    sitemapをpublicに追加

--- sitemap.config.js
+++ sitemap.config.js
@@ -3,5 +3,5 @@ module.exports = {
     siteUrl: process.env.SITE_URL || 'http://localhost:3000',
     generateRobotsTxt: true, // (optional)
     sitemapSize: 7000,
-    outDir: './out',
+    outDir: './public',
 }
